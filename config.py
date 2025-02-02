92911ab9626b66b050a9658ba6c6f6242f1a96ab
harshit-bit10 <109902163+harshit-bit10@users.noreply.github.com>
2025-02-02 21:46:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ class Config(object):
 
     # wes response configuration     
     WEBHOOK = bool(os.environ.get("WEBHOOK", True))
-    PORT = int(os.environ.get("PORT", "8088"))
+    PORT = int(os.environ.get("PORT", "8089"))
 
     caption = """
 **File Name**: {0}
