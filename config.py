a4e2352d61e7fb25fbf836f11a09a4e0699a3ba6
harshit-bit10 <109902163+harshit-bit10@users.noreply.github.com>
2025-02-03 22:21:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config(object):
     # pyro client config
     API_ID    = os.environ.get("API_ID", "7603458")  # ⚠️ Required
     API_HASH  = os.environ.get("API_HASH", "910e420f1f74f40305a684a331dade35") # ⚠️ Required
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7244848756:AAEduZzbV8HVeJc4k0N8VCYui-YVYXNeTGM") # ⚠️ Required
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6302807884:AAHgq_Ctcprr-AFlmxhalSe3NaeoJbyGI8Q") # ⚠️ Required
     FORCE_SUB = os.environ.get('FORCE_SUB', '-1002075434712') # ⚠️ Required
     AUTH_CHANNEL = int(FORCE_SUB) if FORCE_SUB and id_pattern.search(
     FORCE_SUB) else None
